be031fde16d4355b2f7cde889caa8e72401344a2
PushpeshSharma <PushpeshSharma@nextgen.com>
2023-07-17 13:48:34 +0530

    gpt starting plate

---
+++ src/components/brand/imports.js
@@ -0,0 +1,7 @@
+import google from "../../assets/google.png";
+import slack from "../../assets/slack.png";
+import atlassian from "../../assets/atlassian.png";
+import dropbox from "../../assets/dropbox.png";
+import shofiy from "../../assets/shopify.png";
+
+export { google, shofiy, slack, dropbox, atlassian };
